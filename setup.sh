8140bc6524ccbd2b4f2863fccd515c80650b86f1
Benedikt Kantz <benedikt@kantz.at>
2024-08-27 11:11:32 +0000

    start to use selenium

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O ~/miniconda3/miniconda.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-`arch`.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 # restart bash
-conda create -n grapher python=3.11
+conda create -n -y grapher python=3.11
 conda activate grapher
-pip install beautifulsoup4 jupyter
+pip install beautifulsoup4 jupyter python-arango selenium
 sudo apt update
-sudo apt install docker.io docker-compose-v2
+sudo apt install -y docker.io docker-compose-v2  firefox
 sudo usermod -aG docker $USER
 #re-login
+docker compose up -d
+
+git config --global user.email "benedikt@kantz.at"
+git config --global user.name "Benedikt Kantz"
